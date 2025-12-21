b10b42cfbfb7d98eb74efd23c3c6e57c1d3b9ded
lahari-prasanna <200882349+lahari-prasanna@users.noreply.github.com>
2025-12-21 18:24:44 +0530

    Add Listing model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app= express();
 const mongoose=require('mongoose');
 const path=require('path');
+const Listing=require('./models/listing.js');
 
 
 
@@ -9,7 +10,7 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
 
-let MONGO_URL="mongodb://127.0.0.1:27017/WanderLust";
+let MONGO_URL="mongodb://127.0.0.1:27017/VoyageHomes";
 
 main()
     .then(()=>{
@@ -28,6 +29,23 @@ app.get("/",(req,res)=>{
     res.send("Root is working");
 })
 
+app.get("/testListings",(req,res)=>{
+    let sampleListing=new Listing({
+        title:"My new Villa",
+        description:"Nearby beach",
+        price:200,
+        location:"AP",
+        Country:"India"
+    })
+    sampleListing.save()
+        .then(()=>{
+            console.log("listing saved");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    res.send("tested successfully");
+})
 
 app.listen(8080,(req,res)=>{
     console.log("listening to port 8080");

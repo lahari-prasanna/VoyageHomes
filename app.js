3cbd11f4018bb6e8fa2b5ed42b3a355bf98b1a46
lahari-prasanna <200882349+lahari-prasanna@users.noreply.github.com>
2025-12-25 11:58:53 +0530

    Add basic navbar and footer layout

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app= express();
 const mongoose=require('mongoose');
 const path=require('path');
 const Listing=require('./models/listing.js');
-
+const methodOverride= require('method-override');
+const ejsMate=require("ejs-mate");
 
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 let MONGO_URL="mongodb://127.0.0.1:27017/VoyageHomes";
 
@@ -30,23 +33,23 @@ app.get("/",(req,res)=>{
     res.send("Root is working");
 })
 
-app.get("/testListings",(req,res)=>{
-    let sampleListing=new Listing({
-        title:"My new Villa",
-        description:"Nearby beach",
-        price:200,
-        location:"AP",
-        Country:"India"
-    })
-    sampleListing.save()
-        .then(()=>{
-            console.log("listing saved");
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    res.send("tested successfully");
-})
+// app.get("/testListings",(req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My new Villa",
+//         description:"Nearby beach",
+//         price:200,
+//         location:"AP",
+//         Country:"India"
+//     })
+//     sampleListing.save()
+//         .then(()=>{
+//             console.log("listing saved");
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         })
+//     res.send("tested successfully");
+// })
 
 //index Route 
 app.get("/listings",async (req,res)=>{
@@ -55,11 +58,40 @@ app.get("/listings",async (req,res)=>{
         
 })
 
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
 //Show route
 app.get("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("./listings/show.ejs",{listing});
+})
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+
+app.post("/listings",(req,res)=>{
+    const newListing=new Listing(req.body.listing);
+   newListing.save();
+   res.redirect("/listings");
+})
+//UPDATE ROUTE
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//DESTROY ROUTE
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 })
 
 app.listen(8080,(req,res)=>{

81f4fbd70f171becc9f24ce4303d4444930c83cf
lahari-prasanna <200882349+lahari-prasanna@users.noreply.github.com>
2026-02-02 16:00:55 +0530

    refactor: move route logic into controllers

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+module.exports.signUpForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.LoginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+  req.flash("success", "Welcome back to VoyageHomes!");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.signUpForm = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        next(err);
+      }
+      req.flash("success", "Welcome to VoyageHomes!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are Logged out successfully!");
+    res.redirect("/listings");
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,11 @@ const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
+const userController = require("../controllers/user.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+router.get("/signup", userController.signUpForm);
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+router.get("/login", userController.LoginForm);
 
 router.post(
   "/login",
@@ -20,43 +17,11 @@ router.post(
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  async (req, res) => {
-    req.flash("success", "Welcome back to VoyageHomes!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  },
+  userController.login,
 );
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          next(err);
-        }
-        req.flash("success", "Welcome to VoyageHomes!");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  }),
-);
+router.post("/signup", wrapAsync(userController.signUpForm));
 
-router.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "You are Logged out successfully!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
